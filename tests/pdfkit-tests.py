c85c0e2fbd97c1b0c800b601098ad96508a72af3
Stanislav Golovanov <stgolovanov@gmail.com>
2013-01-05 23:58:16 +0400

    refactor and add more tests

--- tests/pdfkit-tests.py
+++ tests/pdfkit-tests.py
@@ -1,49 +1,65 @@
 # -*- coding: utf-8 -*-
 import os
-
+import sys
 import unittest
+
+#Prepend ../ to PYTHONPATH so that we can import PDFKIT form there.
+TESTS_ROOT = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.realpath(os.path.join(TESTS_ROOT, '..')))
 
 import pdfkit
 
-class TestPDFKit(unittest.TestCase):
-    def setUp(self):
-        pass
 
-    def test_html_source_line(self):
+class TestPDFKitInitialization(unittest.TestCase):
+    """Test init"""
+
+    def test_html_source(self):
         r = pdfkit.PDFKit('<h1>Oh hai</h1>', 'string')
         self.assertTrue(r.source.isString())
 
-    def test_url(self):
+    def test_url_source(self):
         r = pdfkit.PDFKit('http://ya.ru', 'url')
         self.assertTrue(r.source.isUrl())
+
+    def test_file_source(self):
+        r = pdfkit.PDFKit('fixtures/example.html', 'file')
+        self.assertTrue(r.source.isFile())
 
     def test_options_parsing(self):
         r = pdfkit.PDFKit('html', 'string', options={'page-size': 'Letter'})
         self.assertTrue(r.options['--page-size'])
 
-    def test_command(self):
+
+class TestPDFKitCommandGeneration(unittest.TestCase):
+    """Test command() method"""
+
+    def test_command_construction(self):
         r = pdfkit.PDFKit('html', 'string', options={'page-size': 'Letter', 'toc-l1-font-size': 12})
         command = r.command()
         self.assertEqual(command[0], r.wkhtmltopdf)
         self.assertEqual(command[command.index('--page-size') + 1], 'Letter')
-        self.assertEqual(command[command.index('--toc-l1-font-size') + 1], '12')
+        self.assertEqual(command[command.index('--toc-l1-font-size') + 1], 12)
 
-    #Not needed, Popen automatically quotes args (?)
-    #    def test_string_quote_encapsulation(self):
-    #        r = pdfkit.PDFKit('html', options={'header-center': 'foo [page]'})
-    #        command = r.command()
-    #        self.assertEqual(command[command.index('--header-center') + 1], '"foo [page]"')
+    def test_lists_of_input_args(self):
+        urls = ['http://ya.ru', 'http://google.com']
+        paths = ['fixtures/example.html', 'fixtures/examples.html']
+        r = pdfkit.PDFKit(urls, 'url')
+        r2 = pdfkit.PDFKit(paths, 'file')
+        cmd = r.command()
+        cmd2 = r2.command()
+        self.assertEqual(cmd[-3:], ['http://ya.ru', 'http://google.com', '-'])
+        self.assertEqual(cmd2[-3:], ['fixtures/example.html', 'fixtures/examples.html', '-'])
 
     def test_read_source_from_stdin(self):
         r = pdfkit.PDFKit('html', 'string')
         self.assertEqual(r.command()[-2:], ['-', '-'])
 
-    def test_url_to_the_source(self):
+    def test_url_in_command(self):
         r = pdfkit.PDFKit('http://ya.ru', 'url')
         self.assertEqual(r.command()[-2:], ['http://ya.ru', '-'])
 
-    def test_path_to_the_file(self):
-        path = 'testfiles/example.html'
+    def test_file_path_in_command(self):
+        path = 'fixtures/example.html'
         r = pdfkit.PDFKit(path, 'file')
         self.assertEqual(r.command()[-2:], [path, '-'])
 
@@ -97,12 +113,6 @@ class TestPDFKit(unittest.TestCase):
         command = r.command()
         self.assertEqual(command[command.index('--orientation') + 1], 'Landscape')
 
-    def test_pdf_generation(self):
-        r = pdfkit.PDFKit('html', 'string', options={'page-size': 'Letter'})
-        pdf = r.to_pdf('ouy.pdf')
-        self.assertEqual(pdf[:4], '%PDF')
-        os.remove('ouy.pdf')
-
     def test_toc_handling_without_options(self):
         r = pdfkit.PDFKit('hmtl', 'string', toc={'xsl-style-sheet': 'test.xsl'})
         self.assertEqual(r.command()[1], 'toc')
@@ -155,59 +165,61 @@ class TestPDFKit(unittest.TestCase):
         command = r.command()
         self.assertEqual(command[-7:], ['toc', '--xsl-style-sheet', 'test.xsl', 'cover', 'test.html', '-', '-'])
 
+
+class TestPDFKitGeneration(unittest.TestCase):
+    """Test to_pdf() method"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        if os.path.exists('out.pdf'):
+            os.remove('out.pdf')
+
+    def test_pdf_generation(self):
+        r = pdfkit.PDFKit('html', 'string', options={'page-size': 'Letter'})
+        pdf = r.to_pdf('out.pdf')
+        self.assertEqual(pdf[:4], '%PDF')
+
     def test_stylesheet_adding_to_the_head(self):
         #TODO rewrite this part of pdfkit.py
-        r = pdfkit.PDFKit('<html><head></head><body>Hai!</body></html>', 'string')
-        css = open('testfiles/example.css')
-        r.stylesheets.append(css)
-        r.to_pdf()
-        self.assertIn('<style>%s</style><html>' % css.read(), r.source.to_s())
+        r = pdfkit.PDFKit('<html><head></head><body>Hai!</body></html>', 'string',
+                          css='fixtures/example.css')
+
+        with open('fixtures/example.css') as f:
+            css = f.read()
+
+        r._prepend_css('fixtures/example.css')
+        self.assertIn('<style>%s</style>' % css, r.source.to_s())
 
     def test_stylesheet_adding_without_head_tag(self):
-        #TODO rewrite this part of pdfkit.py
-        r = pdfkit.PDFKit('<html><body>Hai!</body></html>', 'string')
-        css = open('testfiles/example.css')
-        r.stylesheets.append(css)
-        r.to_pdf()
-        self.assertIn('<style>%s</style><html>' % css.read(), r.source.to_s())
+        r = pdfkit.PDFKit('<html><body>Hai!</body></html>', 'string',
+                          options={'quiet': None}, css='fixtures/example.css')
+
+        with open('fixtures/example.css') as f:
+            css = f.read()
+
+        r._prepend_css('fixtures/example.css')
+        self.assertIn('<style>%s</style><html>' % css, r.source.to_s())
 
     def test_stylesheet_throw_error_when_url(self):
-        r = pdfkit.PDFKit('http://ya.ru', 'url')
-        css = open('testfiles/example.css')
-        r.stylesheets.append(css)
+        r = pdfkit.PDFKit('http://ya.ru', 'url', css='fixtures/example.css')
+
         with self.assertRaises(r.ImproperSourceError):
             r.to_pdf()
 
     def test_stylesheet_adding_to_file_with_option(self):
-        css = 'testfiles/example.css'
-        r = pdfkit.PDFKit('testfiles/example.html', 'file', css=css)
+        css = 'fixtures/example.css'
+        r = pdfkit.PDFKit('fixtures/example.html', 'file', css=css)
         self.assertEqual(r.css, css)
-        self.assertIn('font-size', r._prepend_css(css))
+        r._prepend_css(css)
+        self.assertIn('font-size', r.source.to_s())
 
     def test_raise_error_with_wrong_css_path(self):
-        css = 'testfiles/wrongpath.css'
-        r = pdfkit.PDFKit('testfiles/example.html', 'file', css=css)
+        css = 'fixtures/wrongpath.css'
+        r = pdfkit.PDFKit('fixtures/example.html', 'file', css=css)
         with self.assertRaises(IOError):
             r.to_pdf()
-
-    def test_raise_error_if_css_added_to_url_or_string(self):
-        css = 'testfiles/example.css'
-        r = pdfkit.PDFKit('test', 'string', css=css)
-        r2 = pdfkit.PDFKit('http://google.com', 'url', css=css)
-        with self.assertRaises(r.ImproperSourceError):
-            r._prepend_css(css)
-        with self.assertRaises(r.ImproperSourceError):
-            r2._prepend_css(css)
-
-    def test_lists_of_input_args(self):
-        urls = ['http://ya.ru', 'http://google.com']
-        paths = ['testfiles/example.html', 'testfiles/examples.html']
-        r = pdfkit.PDFKit(urls, 'url')
-        r2 = pdfkit.PDFKit(paths, 'file')
-        cmd = r.command()
-        cmd2 = r2.command()
-        self.assertEqual(cmd[-3:], ['http://ya.ru', 'http://google.com', '-'])
-        self.assertEqual(cmd2[-3:], ['testfiles/example.html', 'testfiles/examples.html', '-'])
 
 if __name__ == "__main__":
     unittest.main()
