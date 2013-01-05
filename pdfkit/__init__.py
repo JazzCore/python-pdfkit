6f1902ff8f79f767b67a194094dbdbe2e565f3a1
Stanislav Golovanov <stgolovanov@gmail.com>
2013-01-05 18:36:21 +0400

    version bump

--- pdfkit/__init__.py
+++ pdfkit/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Golovanov Stanislav'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .pdfkit import PDFKit
 from .api import from_url, from_file, from_string
