23bf02db90f884a953dec3cfd7b6d6eacd116f4f
Stanislav Golovanov <stgolovanov@gmail.com>
2013-01-08 14:06:15 +0400

    version bump

--- HISTORY.rst
+++ HISTORY.rst
@@ -1,6 +1,9 @@
 Changelog
 ---------
 
+* `0.2.4`
+    * Add History
+    * Update setup.py
 * `0.2.3`
     * Fix installing with setup.py
     * Update README
--- pdfkit/__init__.py
+++ pdfkit/__init__.py
@@ -4,7 +4,7 @@ Wkhtmltopdf python wrapper to convert html to pdf using the webkit rendering eng
 """
 
 __author__ = 'Golovanov Stanislav'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 __license__ = 'MIT'
 
 from .pdfkit import PDFKit
