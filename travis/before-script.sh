86bb6a5403707c9b7508dc4b531538be6f5e8577
Stanislav Golovanov <stgolovanov@gmail.com>
2013-01-13 19:27:26 +0400

    another fixes

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -3,5 +3,5 @@
 sudo aptitude install -y openssl build-essential xorg libssl-dev
 wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.11.0_rc1-static-i386.tar.bz2
 tar xvjf wkhtmltopdf-0.11.0_rc1-static-i386.tar.bz2
-chown root:root wkhtmltopdf-i386
-mv wkhtmltopdf-i386 /usr/bin/wkhtmltopdf
+sudo chown root:root wkhtmltopdf-i386
+sudo mv wkhtmltopdf-i386 /usr/bin/wkhtmltopdf
