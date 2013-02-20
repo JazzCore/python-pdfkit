fbf5c79e928e358613558cbc043d982abc1e8ce6
Stanislav Golovanov <stgolovanov@gmail.com>
2013-02-20 22:51:50 +0400

    fix stupid travis error

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -2,6 +2,6 @@
 
 sudo aptitude install -y openssl build-essential xorg libssl-dev
 wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.10.0_rc2-static-i386.tar.bz2
-tar xvjf wkhtmltopdf-0.11.0_rc1-static-i386.tar.bz2
+tar xvjf wkhtmltopdf-0.10.0_rc2-static-i386.tar.bz2
 sudo chown root:root wkhtmltopdf-i386
 sudo mv wkhtmltopdf-i386 /usr/bin/wkhtmltopdf
