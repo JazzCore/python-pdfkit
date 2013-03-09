0add46e14e1b3fcb013110c735dfe57a796c736c
Stanislav Golovanov <stgolovanov@gmail.com>
2013-03-09 16:16:03 +0400

    fix travis builds by using 64 bit binary

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo aptitude install -y openssl build-essential xorg libssl-dev
-wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.10.0_rc2-static-i386.tar.bz2
-tar xvjf wkhtmltopdf-0.10.0_rc2-static-i386.tar.bz2
-sudo chown root:root wkhtmltopdf-i386
-sudo mv wkhtmltopdf-i386 /usr/bin/wkhtmltopdf
+sudo apt-get install -y openssl build-essential xorg libssl-dev
+wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2
+tar xvjf wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2
+sudo chown root:root wkhtmltopdf-amd64
+sudo mv wkhtmltopdf-amd64 /usr/bin/wkhtmltopdf
