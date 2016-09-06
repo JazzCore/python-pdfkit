89956729cbbd0164478ad62a31ac94abc1a797fa
Stanislav Golovanov <stgolovanov@gmail.com>
2016-09-06 19:39:46 +0300

    Fix Travis builds

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 sudo apt-get install -y openssl build-essential xorg libssl-dev
-wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2
-tar xvjf wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2
-sudo chown root:root wkhtmltopdf-amd64
-sudo mv wkhtmltopdf-amd64 /usr/bin/wkhtmltopdf
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+tar -xJf wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+cd wkhtmltox
+sudo chown root:root bin/wkhtmltopdf
+sudo cp -r * /usr/
