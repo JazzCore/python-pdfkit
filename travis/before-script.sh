214255312922c9448a28300dd725596c18e83aff
Stanislav Golovanov <stgolovanov@gmail.com>
2021-03-20 00:33:37 +0300

    More Travis updates

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -2,8 +2,7 @@
 
 WKHTML2PDF_VERSION='0.12.6-1'
 
-sudo apt-get install -y openssl build-essential xorg libssl-dev libxrender1 libfontconfig1 libx11-dev libjpeg62 libxtst6 fontconfig xfonts-75dpi xfonts-base libpng12-0
-https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
-wget "https://github.com/wkhtmltopdf/packaging/releases/download/${WKHTML2PDF_VERSION}/wkhtmltox_${WKHTML2PDF_VERSION}.focal_amd64.deb"
-sudo dpkg -i wkhtmltox_${WKHTML2PDF_VERSION}.focal_amd64.deb
+sudo apt-get install -y openssl build-essential libssl-dev libpthread-stubs0-dev libxau-dev xorg-sgml-doctools libxdmcp-dev x11proto-core-dev x11proto-input-dev x11proto-kb-dev xtrans-dev libx11-dev libxcb1-dev libjpeg62 libxtst6 libfontenc1 libxfont1 x11-common xfonts-encodings xfonts-utils fontconfig xfonts-base xfonts-75dpi
+wget "https://github.com/wkhtmltopdf/packaging/releases/download/${WKHTML2PDF_VERSION}/wkhtmltox_${WKHTML2PDF_VERSION}.xenial_amd64.deb"
+sudo dpkg -i wkhtmltox_${WKHTML2PDF_VERSION}.xenial_amd64.deb
 sudo apt-get -f install
