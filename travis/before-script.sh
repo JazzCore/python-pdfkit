ec22673d4e31f317ff50aa4664024f3afc23bb7e
Stanislav Golovanov <stgolovanov@gmail.com>
2021-03-20 01:05:15 +0300

    Travis still fails

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -2,7 +2,6 @@
 
 WKHTML2PDF_VERSION='0.12.6-1'
 
-sudo apt-get install -y openssl build-essential libssl-dev libxrender1 libfontconfig1 libx11-dev fontconfig xfonts-75dpi xfonts-base
+sudo apt-get install -y build-essential xorg libssl-dev libxrender-dev wget gdebi
 wget "https://github.com/wkhtmltopdf/packaging/releases/download/${WKHTML2PDF_VERSION}/wkhtmltox_${WKHTML2PDF_VERSION}.bionic_amd64.deb"
-sudo dpkg -i wkhtmltox_${WKHTML2PDF_VERSION}.bionic_amd64.deb
-sudo apt-get -f install
+sudo gdebi --n wkhtmltox_${WKHTML2PDF_VERSION}.bionic_amd64.deb
