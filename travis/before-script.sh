ea99799eaa5751348888ee59233db1ca95917c8a
Stanislav Golovanov <stgolovanov@gmail.com>
2021-03-19 23:58:21 +0300

    update travis wkhtmltopdf version to 0.12.6-1

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 
-WKHTML2PDF_VERSION='0.12.4'
+WKHTML2PDF_VERSION='0.12.6-1'
 
 sudo apt-get install -y openssl build-essential xorg libssl-dev
-wget "https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/${WKHTML2PDF_VERSION}/wkhtmltox-${WKHTML2PDF_VERSION}_linux-generic-amd64.tar.xz"
-tar -xJf "wkhtmltox-${WKHTML2PDF_VERSION}_linux-generic-amd64.tar.xz"
-cd wkhtmltox
-sudo chown root:root bin/wkhtmltopdf
-sudo cp -r * /usr/
+https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
+wget "https://github.com/wkhtmltopdf/packaging/releases/download/${WKHTML2PDF_VERSION}/wkhtmltox_${WKHTML2PDF_VERSION}.focal_amd64.deb"
+sudo dpkg -i wkhtmltox_${WKHTML2PDF_VERSION}.focal_amd64.deb
