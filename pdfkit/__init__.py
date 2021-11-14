80956064ac71e517da4a45e1b7fca3d751637b6e
Stanislav Golovanov <stgolovanov@gmail.com>
2021-11-14 21:23:58 +0300

    bump version to 1.0.0

--- pdfkit/__init__.py
+++ pdfkit/__init__.py
@@ -4,7 +4,7 @@ Wkhtmltopdf python wrapper to convert html to pdf using the webkit rendering eng
 """
 
 __author__ = 'Golovanov Stanislav'
-__version__ = '0.6.1'
+__version__ = '1.0.0'
 __license__ = 'MIT'
 
 from .pdfkit import PDFKit
